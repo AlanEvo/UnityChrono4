092c6446384a66e5f23731d5e4b5d2e43e574df1
Alan Weaver <alan.weaver@solent-renegades.co.uk>
2021-03-27 14:58:20 +0000

    Added Tooltips to ChBody AuxRef and LinkLock

--- Assets/Scripts/Physics/ChBody.cs
+++ Assets/Scripts/Physics/ChBody.cs
@@ -21,7 +21,8 @@ namespace chrono
     public class ChBody : ChPhysicsItem, ChContactable_1vars<IntInterface.Six>, IChLoadableUVW
     {
 
-        public ChBodyFrame BodyFrame;// = new ChBodyFrame();
+        // Alan // TO DO make both BodyFrame and collision_model protected and create a GetBody() and GetCollisionModel()
+        public ChBodyFrame BodyFrame;
 
         public collision.ChCollisionModel collision_model;  //< pointer to the collision model
 
@@ -297,7 +298,7 @@ namespace chrono
 
             collision_model = InstanceCollisionModel();
 
-            //density = density;
+            density = 1000.0f;
 
             max_speed = 0.5f;
             max_wvel = 2.0f * (float)(ChMaths.CH_C_PI);
@@ -335,7 +336,7 @@ namespace chrono
 
             collision_model = InstanceCollisionModel();
 
-           // density = 1000.0f;
+            density = 1000.0f;
 
             max_speed = 0.5f;
             max_wvel = 2.0f * (float)(ChMaths.CH_C_PI);
@@ -385,7 +386,7 @@ namespace chrono
                      break;
              }*/
 
-           // density = 1000.0f;
+            density = 1000.0f;
 
             last_coll_pos = new ChCoordsys<double>(new ChVector(), new ChQuaternion(0, 0, 0, 0));
 
@@ -1931,51 +1932,78 @@ namespace chrono
         {
             ChBody body = (ChBody)target;
 
-            body.type = (ChBody.CollisionType)EditorGUILayout.EnumPopup("Collision Type", body.type);
-            body.bodyfixed = EditorGUILayout.Toggle("Fixed", body.bodyfixed);
-            body.collide = EditorGUILayout.Toggle("Collide", body.collide);
-            
-            body.automaticMass = EditorGUILayout.Toggle("Automatic Mass/Inertia", body.automaticMass);
+            EditorGUILayout.HelpBox("Class for rigid bodies. A rigid body is an entity which can move in 3D space, and can be constrained to other rigid bodies using ChLink objects.", MessageType.Info);
 
-            if (!body.automaticMass) { 
-               // body.CalculateMassProperties();
-                body.mass = EditorGUILayout.DoubleField("Mass", body.mass);
-               // body.COM = EditorGUILayout.Vector3Field("Center of Mass", body.COM);
-                body.inertiaMoments = EditorGUILayout.Vector3Field("Moments of Inertia", body.inertiaMoments);
-                body.inertiaProducts = EditorGUILayout.Vector3Field("Products of Inertia", body.inertiaProducts);
+            GUIContent type = new GUIContent("Collision Type", "Change what type of collision shape.");
+            body.type = (ChBody.CollisionType)EditorGUILayout.EnumPopup(type, body.type);
+            GUIContent bodyfixed = new GUIContent("Fixed", "Toggle whether the object is a static/fixed rigid body or dynamic.");
+            body.bodyfixed = EditorGUILayout.Toggle(bodyfixed, body.bodyfixed);
+            GUIContent collide = new GUIContent("Collide", "Toggle whether the rigid body can collide and interact with other collidable rigid body.");
+            body.collide = EditorGUILayout.Toggle(collide, body.collide);
+
+            GUIContent autoInertia = new GUIContent("Automatic Mass/Inertia", "Toggle whether the rigid body's mass and inertia is calculated automatically based on dimensions and custom density, or if you would like to set the mass and inertia manually.");
+            body.automaticMass = EditorGUILayout.Toggle(autoInertia, body.automaticMass);
+
+            if (!body.automaticMass) {
+
+                GUIContent mass = new GUIContent("Mass", "Mass of the rigid body. Must be positive. Try not to mix bodies with too high/too low values of mass, for numerical stability.");
+                body.mass = EditorGUILayout.DoubleField(mass, body.mass);
+                GUIContent inertXX = new GUIContent("Moments of Inertia", "Advanced, Set the diagonal part of the inertia tensor, The provided 3x1 vector should contain the moments of inertia, expressed in the local coordinate frame");
+                body.inertiaMoments = EditorGUILayout.Vector3Field(inertXX, body.inertiaMoments);
+                GUIContent inertXY = new GUIContent("Products of Inertia", "Advanced, Set the off-diagonal part of the inertia tensor (Ixy, Ixz, Iyz values). Warning about sign: in some books they write the inertia tensor as I=[Ixx, -Ixy, -Ixz; etc.] but here is I=[Ixx, Ixy, Ixz; ...]. The provided 3x1 vector should contain the products of inertia, expressed in the local coordinate frame:");
+                body.inertiaProducts = EditorGUILayout.Vector3Field(inertXY, body.inertiaProducts);
             }
             else
             {
-                EditorGUILayout.PropertyField(density, new GUIContent("MDensity"), GUILayout.Height(20));
+                GUIContent density = new GUIContent("MDensity", "Set the rigid body density. Used when doing the 'recompute mass' operation.");
+                body.density = EditorGUILayout.FloatField(density, body.density);
             }
 
             EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
 
-            body.materialType =(ChBody.MaterialType)EditorGUILayout.EnumPopup("Material Type", body.materialType);
+            GUIContent linVel = new GUIContent("Linear Velocity", "Set the linear speed.  This is executed at startup once.");
+            body.linearVelocity = EditorGUILayout.Vector3Field(linVel, body.linearVelocity);
+            GUIContent angVel = new GUIContent("Angular Velocity", "Set the rotation speed from given angular speed.  This is executed at startup once.");
+            body.angularVelocity = EditorGUILayout.Vector3Field(angVel, body.angularVelocity);
+
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+            GUIContent matType = new GUIContent("Material Type", "Change the material physics type. NSC surface for use with non-smooth (complementarity) contact method.  SMC Material data for a surface for use with smooth (penalty) contact method. This data is used to define surface properties owned by ChBody rigid bodies and similar objects; it carries information that is used to make contacts.");
+            body.materialType =(ChBody.MaterialType)EditorGUILayout.EnumPopup(matType, body.materialType);
 
             EditorGUI.indentLevel++;
             switch (body.materialType)
             {
                 case ChBody.MaterialType.NSC:
                     EditorGUILayout.PropertyField(friction, new GUIContent("Friction"), GUILayout.Height(20));
-                   // EditorGUILayout.PropertyField(sliding_friction, new GUIContent("Sliding Friction"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(rolling_friction, new GUIContent("Rolling Friction"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(spinning_friction, new GUIContent("Spinning Friction"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(restitution, new GUIContent("Restitution"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(cohesion, new GUIContent("Cohesion"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(dampingf, new GUIContent("Dampingf"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(compliance, new GUIContent("Compliance"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(complianceT, new GUIContent("ComplianceT"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(complianceRoll, new GUIContent("Compliance Roll"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(complianceSpin, new GUIContent("Compliance Spin"), GUILayout.Height(20));
+                    GUIContent rolling = new GUIContent("Rolling Friction", "The rolling friction (rolling parameter, it has the dimension of a length). Rolling resistant torque is Tr <= (normal force) * (this parameter) Usually a very low value. Measuring unit: m  Default =0. Note! a non-zero value will make the simulation 2x slower! Also, the GPU solver currently does not support rolling friction. Default: 0.");
+                    EditorGUILayout.PropertyField(rolling_friction, new GUIContent(rolling), GUILayout.Height(20));
+                    GUIContent spinning = new GUIContent("Spinning Friction", "The spinning friction (it has the dimension of a length). Spinning resistant torque is Ts <= (normal force) * (this parameter) Usually a very low value.  Measuring unit: m Default =0. Note! a non-zero value will make the simulation 2x slower! Also, the GPU solver currently does not support spinning friction. Default: 0.");
+                    EditorGUILayout.PropertyField(spinning_friction, new GUIContent(spinning), GUILayout.Height(20));
+                    GUIContent rest = new GUIContent("Restitution", "The normal restitution coefficient, for collisions. Should be in 0..1 range. Default =0.");
+                    EditorGUILayout.PropertyField(restitution, new GUIContent(rest), GUILayout.Height(20));
+                    GUIContent coh = new GUIContent("Cohesion", "The cohesion max. force for normal pulling traction in contacts. Measuring unit: N Default =0.");
+                    EditorGUILayout.PropertyField(cohesion, new GUIContent(coh), GUILayout.Height(20));
+                    GUIContent dampf = new GUIContent("Dampingf", "The damping in contact, as a factor 'f': damping is a multiple of stiffness [K], that is: [R]=f*[K] Measuring unit: time, s. Default =0.");
+                    EditorGUILayout.PropertyField(dampingf, new GUIContent(dampf), GUILayout.Height(20));
+                    GUIContent comp = new GUIContent("Compliance", "Compliance of the contact, in normal direction. It is the inverse of the stiffness [K] , so for zero value one has a perfectly rigid contact. Measuring unit: m/N Default =0.");
+                    EditorGUILayout.PropertyField(compliance, new GUIContent(comp), GUILayout.Height(20));
+                    GUIContent compt = new GUIContent("ComplianceT", "Compliance of the contact, in normal direction. It is the inverse of the stiffness [K] , so for zero value one has a perfectly rigid contact. Measuring unit: m/N Default =0.");
+                    EditorGUILayout.PropertyField(complianceT, new GUIContent(compt), GUILayout.Height(20));
+                    GUIContent comproll = new GUIContent("Compliance Rolling", "Rolling compliance of the contact, if using a nonzero rolling friction. (If there is no rolling friction, this has no effect.) Measuring unit: rad/Nm Default =0.");
+                    EditorGUILayout.PropertyField(complianceRoll, new GUIContent(comproll), GUILayout.Height(20));
+                    GUIContent compspin = new GUIContent("Compliance Spinning", " Spinning compliance of the contact, if using a nonzero rolling friction. (If there is no spinning friction, this has no effect.) Measuring unit: rad/Nm Default =0.");
+                    EditorGUILayout.PropertyField(complianceSpin, new GUIContent(compspin), GUILayout.Height(20));
 
                     serializedObject.ApplyModifiedProperties();
 
                     //body.matsurface = body.gameObject.AddComponent<ChMaterialSurfaceNSC>() as ChMaterialSurfaceNSC;
                     break;
                 case ChBody.MaterialType.SMC:
-                    EditorGUILayout.PropertyField(young_modulus, new GUIContent("Young Modulus"), GUILayout.Height(20));
-                    EditorGUILayout.PropertyField(poisson_ratio, new GUIContent("Poisson Ratio"), GUILayout.Height(20));
+                    GUIContent young = new GUIContent("Young Modulus", "Young's modulus, modulus of elasticity");
+                    EditorGUILayout.PropertyField(young_modulus, new GUIContent(young), GUILayout.Height(20));
+                    GUIContent poison = new GUIContent("Poisson Ratio", "Poisson's ratio is a measure of the Poisson effect, the phenomenon in which a material tends to expand in directions perpendicular to the direction of compression. Conversely, if the material is stretched rather than compressed, it usually tends to contract in the directions transverse to the direction of stretching.");
+                    EditorGUILayout.PropertyField(poisson_ratio, new GUIContent(poison), GUILayout.Height(20));
                     EditorGUILayout.PropertyField(static_friction, new GUIContent("Static Friction"), GUILayout.Height(20));
                     EditorGUILayout.PropertyField(sliding_friction, new GUIContent("Sliding Friction"), GUILayout.Height(20));
                     EditorGUILayout.PropertyField(constant_adhesion, new GUIContent("Constant Adhesion"), GUILayout.Height(20));
--- Assets/Scripts/Physics/ChLinkLock.cs
+++ Assets/Scripts/Physics/ChLinkLock.cs
@@ -64,6 +64,7 @@ namespace chrono
         protected ChLinkLimit limit_Rp;// = new ChLinkLimit();  //< the polar (conical) limit for "shoulder"rotation
         protected ChLinkLimit limit_D;// = new ChLinkLimit();   //< the polar (conical) limit for "shoulder"rotation
 
+        
         public ChBody body1;
         public ChBody body2;
 
@@ -2582,24 +2583,79 @@ namespace chrono
       
     }
 
-  /*  [CustomEditor(typeof(ChLinkLock))]
+    [CustomEditor(typeof(ChLinkLock))]
     public class UChJointRevoluteEditor : Editor
     {
         override public void OnInspectorGUI()
         {
             var joint = target as ChLinkLock;
 
-            joint.type = (ChLinkLock.LinkType)EditorGUILayout.EnumPopup("Joint Type", joint.type);
-            joint.body1 = (ChBody)EditorGUILayout.ObjectField("Body 1", joint.body1, typeof(ChBody), true);
-            joint.body2 = (ChBody)EditorGUILayout.ObjectField("Body 2", joint.body2, typeof(ChBody), true);
+            EditorGUILayout.HelpBox("ChLinkLock class. This class implements lot of sub types like the revolute joint, the linear guide, the spherical joint, etc. using the 'lock formulation'. Also, it optionally allows the adoption of 'limits' over upper-lower motions on all the 6 degrees of freedom, thank to the ChLinkLimit objects.", MessageType.Info);
 
+            GUIContent type = new GUIContent("Link Type", "Change what type of joint you want.");
+            joint.type = (ChLinkLock.LinkType)EditorGUILayout.EnumPopup(type, joint.type);
+            GUIContent body1 = new GUIContent("Body 1", "First connected body.");
+            joint.body1 = (ChBody)EditorGUILayout.ObjectField(body1, joint.body1, typeof(ChBody), true);
+            GUIContent body2 = new GUIContent("Body 2", "Second connected body.");
+            joint.body2 = (ChBody)EditorGUILayout.ObjectField(body2, joint.body2, typeof(ChBody), true);
+
+            GUIContent limit = new GUIContent("Joint Limits", "Toggle if the joint is to have upper and lower limits.");
             joint.enableLimits = EditorGUILayout.Toggle("Joint Limits", joint.enableLimits);
 
             if (joint.enableLimits)
             {
-                EditorGUI.indentLevel++;
-                joint.minAngle = EditorGUILayout.DoubleField("Min Angle", joint.minAngle);
-                joint.maxAngle = EditorGUILayout.DoubleField("Max Angle", joint.maxAngle);
+                switch (joint.type)
+                {
+                    case ChLinkLock.LinkType.FREE:
+
+                        break;
+                    case ChLinkLock.LinkType.LOCK:
+
+                        break;
+                    case ChLinkLock.LinkType.SPHERICAL:
+
+                        break;
+                    case ChLinkLock.LinkType.POINTPLANE:
+
+                        break;
+                    case ChLinkLock.LinkType.POINTLINE:
+
+                        break;
+                    case ChLinkLock.LinkType.REVOLUTE:
+                        EditorGUI.indentLevel++;
+                        joint.minAngle = EditorGUILayout.DoubleField("Min Angle", joint.minAngle);
+                        joint.maxAngle = EditorGUILayout.DoubleField("Max Angle", joint.maxAngle);
+                        break;
+                    case ChLinkLock.LinkType.CYLINDRICAL:
+
+                        break;
+                    case ChLinkLock.LinkType.PRISMATIC:
+                        EditorGUI.indentLevel++;
+                        joint.minDisplacement = EditorGUILayout.DoubleField("Min Displacement", joint.minDisplacement);
+                        joint.maxDisplacement = EditorGUILayout.DoubleField("Max Displacement", joint.maxDisplacement);
+                        break;
+                    case ChLinkLock.LinkType.PLANEPLANE:
+
+                        break;
+                    case ChLinkLock.LinkType.OLDHAM:
+
+                        break;
+                    case ChLinkLock.LinkType.ALIGN:
+
+                        break;
+                    case ChLinkLock.LinkType.PARALLEL:
+
+                        break;
+                    case ChLinkLock.LinkType.PERPEND:
+
+                        break;
+                    case ChLinkLock.LinkType.REVOLUTEPRISMATIC:
+
+                        break;
+                    default:
+
+                        break;
+                }
                 EditorGUI.indentLevel--;
             }
 
@@ -2610,6 +2666,6 @@ namespace chrono
                 EditorUtility.SetDirty(joint);
             }
         }
-    }*/
+    }
 
 }
