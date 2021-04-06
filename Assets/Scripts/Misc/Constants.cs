08c37175b94af627f4da0e4fb84c797f95d701cc
Alan Weaver <alan.weaver@solent-renegades.co.uk>
2021-04-06 14:57:15 +0100

    Initial pull

--- Assets/Scripts/Misc/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Constants
-{
-   public enum Direction { NONE, IN, OUT, UP, DOWN, FORWARD, BACKWARD, LEFT, RIGHT };
-
-}
